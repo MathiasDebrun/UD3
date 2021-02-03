817bf524868669a244249c02c8d41c15baed1bf0
MathiasDebrun <mathiasdebrun@hotmail.com>
2021-02-03 16:01:12 +0100

    Commit

--- UD4/src/Partedos.java
+++ UD4/src/Partedos.java
@@ -5,14 +5,17 @@ public class Partedos {
 		// TODO Auto-generated method stub
 		//Parte dos
 		
-				int N=5;
-				double A=4.56;
-				char C='a';
+				int N=10;
+				double A=8.26;
+				char C=64;
 				
 				
 				System.out.println("Variable N= " +N);
 				System.out.println("Variable A= " +A);
-
+                System.out.println("Variable C= " +C);
+                System.out.println("Suma de N+A es igual a: " +(N+A));
+                System.out.println("La diferencia entre N y A es: " +(N-A));
+                System.out.println("El valor numérico de " +C +" es: 64");
 	}
 
 }
